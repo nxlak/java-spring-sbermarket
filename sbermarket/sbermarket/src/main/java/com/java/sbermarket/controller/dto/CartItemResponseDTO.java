8ebce4eb58dfa3b71270139d5fadc72a4d432933
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:20:56 +0300

    Update CartItemResponseDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/CartItemResponseDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/CartItemResponseDTO.java
@@ -2,6 +2,13 @@ package com.java.sbermarket.controller.dto;
 
 import com.java.sbermarket.entity.CartItem;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItemResponseDTO {
 
     private Long cartItemId;
@@ -10,41 +17,10 @@ public class CartItemResponseDTO {
 
     private Integer amount;
 
-    public CartItemResponseDTO(Long cartItemId, ProductResponseDTO product, Integer amount) {
-        this.cartItemId = cartItemId;
-        this.product = product;
-        this.amount = amount;
-    }
-
     public CartItemResponseDTO(CartItem cartItem) {
         this.cartItemId = cartItem.getCartItemId();
         this.product = new ProductResponseDTO(cartItem.getProduct());
         this.amount = cartItem.getAmount(); 
     }
 
-    public Long getCartItemId() {
-        return cartItemId;
-    }
-
-    public void setCartItemId(Long cartItemId) {
-        this.cartItemId = cartItemId;
-    }
-
-    public ProductResponseDTO getProduct() {
-        return product;
-    }
-
-    public void setProduct(ProductResponseDTO product) {
-        this.product = product;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    
 }

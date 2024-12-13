d61c85666b1e29e820919dd173d404d7d6f1d64a
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:21:53 +0300

    Update OrderItemRequestDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/OrderItemRequestDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/OrderItemRequestDTO.java
@@ -3,26 +3,17 @@ package com.java.sbermarket.controller.dto;
 import com.java.sbermarket.entity.Order;
 import com.java.sbermarket.entity.OrderItem;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItemRequestDTO {
     
     private Long productId;
     private Integer amount;
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
 
     public OrderItem toEntity(Order order) {
         OrderItem orderItem = new OrderItem();

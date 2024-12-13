1b925293a7cc91d03830f9d71e0c9f5d6322e8d3
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:22:39 +0300

    Update PayMethodRequestDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/PayMethodRequestDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/PayMethodRequestDTO.java
@@ -2,6 +2,13 @@ package com.java.sbermarket.controller.dto;
 
 import com.java.sbermarket.entity.PayMethod;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PayMethodRequestDTO {
 
     private String name;
@@ -13,46 +20,6 @@ public class PayMethodRequestDTO {
     private String expr_date;
 
     private String cvv;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBank_name() {
-        return bank_name;
-    }
-
-    public void setBank_name(String bank_name) {
-        this.bank_name = bank_name;
-    }
-
-    public String getCard_num() {
-        return card_num;
-    }
-
-    public void setCard_num(String card_num) {
-        this.card_num = card_num;
-    }
-
-    public String getExpr_date() {
-        return expr_date;
-    }
-
-    public void setExpr_date(String expr_date) {
-        this.expr_date = expr_date;
-    }
-
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
 
     public PayMethod toEntity() {
         PayMethod payMethod = new PayMethod();

7616964674db8765185d7cd1785d486ba657a0f6
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:21:41 +0300

    Update FavouritesResponseDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/FavouritesResponseDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/FavouritesResponseDTO.java
@@ -5,37 +5,22 @@ import java.util.stream.Collectors;
 
 import com.java.sbermarket.entity.Favourites;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class FavouritesResponseDTO {
     
     private Long id;
 
     private List<ProductResponseDTO> products;
 
-    public FavouritesResponseDTO(Long id, List<ProductResponseDTO> product) {
-        this.id = id;
-        this.products = product;
-    }
-
     public FavouritesResponseDTO(Favourites favourites) {
         this.id = favourites.getId();
         this.products = favourites.getProducts().stream().map(ProductResponseDTO::new).collect(Collectors.toList());
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public List<ProductResponseDTO> getProducts() {
-        return products;
-    }
-
-    public void setProducts(List<ProductResponseDTO> products) {
-        this.products = products;
-    }
-
-    
 }

07535f16bff24cb244c5422b6f9f70711ca9c376
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:20:46 +0300

    Update CartItemRequestDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/CartItemRequestDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/CartItemRequestDTO.java
@@ -2,43 +2,21 @@ package com.java.sbermarket.controller.dto;
 
 import com.java.sbermarket.entity.CartItem;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItemRequestDTO {
-    
-    //private Long cartItemId;
 
     private ProductRequestDTO product;
 
     private Integer amount;
 
-    /*
-    public Long getCartItemId() {
-        return cartItemId;
-    }
-
-    public void setCartItemId(Long cartItemId) {
-        this.cartItemId = cartItemId;
-    }
-    */
-
-    public ProductRequestDTO getProduct() {
-        return product;
-    }
-
-    public void setProduct(ProductRequestDTO product) {
-        this.product = product;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
     public CartItem toEntity() {
         CartItem cartItem = new CartItem();
-        //cartItem.setCartItemId(this.getCartItemId());
         cartItem.setProduct(this.getProduct().toEntity());
         cartItem.setAmount(this.getAmount());
         return cartItem;

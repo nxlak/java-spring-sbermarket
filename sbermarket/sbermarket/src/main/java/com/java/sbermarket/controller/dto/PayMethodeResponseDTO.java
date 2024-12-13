9d4e3c65146c4a1ec829cfe84420b44fde0a16d3
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:22:50 +0300

    Update PayMethodeResponseDTO.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/PayMethodeResponseDTO.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/controller/dto/PayMethodeResponseDTO.java
@@ -2,6 +2,13 @@ package com.java.sbermarket.controller.dto;
 
 import com.java.sbermarket.entity.PayMethod;
 
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class PayMethodeResponseDTO {
     
     private Long payMethodId;
@@ -25,61 +32,4 @@ public class PayMethodeResponseDTO {
         this.cvv = payMethod.getCvv();
     }
 
-    public PayMethodeResponseDTO(Long payMethodId, String name, String bank_name, String card_num, String expr_date,
-            String cvv) {
-        this.payMethodId = payMethodId;
-        this.name = name;
-        this.bank_name = bank_name;
-        this.card_num = card_num;
-        this.expr_date = expr_date;
-        this.cvv = cvv;
-    }
-
-    public Long getPayMethodId() {
-        return payMethodId;
-    }
-
-    public void setPayMethodId(Long payMethodId) {
-        this.payMethodId = payMethodId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBank_name() {
-        return bank_name;
-    }
-
-    public void setBank_name(String bank_name) {
-        this.bank_name = bank_name;
-    }
-
-    public String getCard_num() {
-        return card_num;
-    }
-
-    public void setCard_num(String card_num) {
-        this.card_num = card_num;
-    }
-
-    public String getExpr_date() {
-        return expr_date;
-    }
-
-    public void setExpr_date(String expr_date) {
-        this.expr_date = expr_date;
-    }
-
-    public String getCvv() {
-        return cvv;
-    }
-
-    public void setCvv(String cvv) {
-        this.cvv = cvv;
-    }
 }

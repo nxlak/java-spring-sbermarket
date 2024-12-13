6a4a86e874102bb309fa5b1b530bcee4ef615b5f
nxlak <161253470+nxlak@users.noreply.github.com>
2024-12-13 23:25:09 +0300

    Update Product.java

--- sbermarket/sbermarket/src/main/java/com/java/sbermarket/entity/Product.java
+++ sbermarket/sbermarket/src/main/java/com/java/sbermarket/entity/Product.java
@@ -6,8 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.*;
 
 @Entity
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@Builder
 public class Product {
     
     @Id
@@ -18,62 +23,5 @@ public class Product {
     private String photo_source;
     private BigDecimal weight;
     private BigDecimal price;
-
-    public Product(String name, String photo_source, BigDecimal weight, BigDecimal price) {
-        this.name = name;
-        this.photo_source = photo_source;
-        this.weight = weight;
-        this.price = price;
-    }
-
-    public Product() {
-        
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-    
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoto_source() {
-        return photo_source;
-    }
-
-    public void setPhoto_source(String photo_source) {
-        this.photo_source = photo_source;
-    }
-
-    public BigDecimal getWeight() {
-        return weight;
-    }
-
-    public void setWeight(BigDecimal weight) {
-        this.weight = weight;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    @Override
-    public String toString() {
-        return "Product [productId=" + productId + ", name=" + name + ", photo_source=" + photo_source + ", weight="
-                + weight + ", price=" + price + "]";
-    }
 
 }
